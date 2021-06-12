27dfc9eda42734a8909ddc33dbc281eb2412c38c
Alejandro De Lio <alejandro.delio@munitienda.com>
2021-06-11 21:51:14 -0300

    real initial commit

---
+++ src/fors.js
@@ -0,0 +1,9 @@
+/**
+ * must print to console from 1 to 20
+ * (ie: console.log(1), console.log(2), console.log(3))
+ */
+function printToConsole1to20(console) {
+    // TODO
+}
+
+module.exports = printToConsole1to20;
---
+++ src/fors.test.js
@@ -0,0 +1,14 @@
+const printToConsole1to20 = require('./fors');
+
+test('printToConsole1to20', () => {
+    const console = {
+        arr: [],
+        log: function (msg) {
+            this.arr.push(msg);
+        },
+    }
+    printToConsole1to20(console);
+    expect(console.arr).toEqual([
+        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+    ]);
+});
