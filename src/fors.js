224e920bf0f2a813268cb05871e9caeecf6e9393
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-06-12 13:30:34 -0300

    finalizacion ejercicio

--- src/fors.js
+++ src/fors.js
@@ -3,7 +3,13 @@
  * (ie: console.log(1), console.log(2), console.log(3))
  */
 function printToConsole1to20(console) {
-    // TODO
+
+        for (let i = 1; i < 21;i++) {
+            console.log(i)
+        }
+
+    return console.arr;
+
 }
 
 module.exports = printToConsole1to20;
