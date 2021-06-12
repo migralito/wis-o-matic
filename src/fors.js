468de0a3776f8904d4322b8e6531a4bce1b02ec1
Alejandro De Lio <alejandro.delio@munitienda.com>
2021-06-12 13:59:19 -0300

    fors: new exercises

--- src/fors.js
+++ src/fors.js
@@ -2,14 +2,75 @@
  * must print to console from 1 to 20
  * (ie: console.log(1), console.log(2), console.log(3))
  */
-function printToConsole1to20(console) {
+exports.print1To20 = function(console) {
+    for (let i = 1; i < 21;i++) {
+        console.log(i)
+    }
 
-        for (let i = 1; i < 21;i++) {
-            console.log(i)
-        }
+    // return console.arr;  <== not necessary
+};
 
-    return console.arr;
-
+/**
+ * Print to console a descending sequence from 20 to 1.
+ *
+ * ### Expected behavior
+ * ```
+ * console.log(20);
+ * console.log(19);
+ * ...
+ * console.log(1);
+ * ```
+ *
+ * ### Resources to use
+ * - only for statment
+ *
+ * ### Estimated amount of time needed to complete the task
+ * 2 minutes
+ */
+exports.print20To1 = function(console) {
+    // TODO
 }
 
-module.exports = printToConsole1to20;
+/**
+ * Print to console a sequence starting from 1, increasing by 3, until 28.
+ *
+ * ### Expected behavior
+ * ```
+ * console.log(1);
+ * console.log(4);
+ * console.log(7);
+ * ...
+ * console.log(28);
+ * ```
+ *
+ * ### Resources to use
+ * - only for statment
+ *
+ * ### Estimated amount of time needed to complete the task
+ * 5 minutes
+ */
+exports.print1To28Step3 = function(console) {
+    // TODO
+}
+
+/**
+ * Print to console a descending sequence sequence starting from 30, decreasing by 4, stopping at 6.
+ *
+ * ### Expected behavior
+ * ```
+ * console.log(30);
+ * console.log(26);
+ * console.log(22);
+ * ...
+ * console.log(6);
+ * ```
+ *
+ * ### Resources to use
+ * - only for statment
+ *
+ * ### Estimated amount of time needed to complete the task
+ * 5 minutes
+ */
+exports.printFrom30To6Step3Descending = function(console) {
+    // TODO
+}
--- src/fors.test.js
+++ src/fors.test.js
@@ -1,16 +1,51 @@
-const printToConsole1to20 = require('./fors');
+const fors = require('./fors');
 
-test('printToConsole1to20', () => {
-    const console = {
-        arr: [],
-        log: function (msg) {
-            this.arr.push(msg);
-        },
-    }
-    printToConsole1to20(console);
-    expect(console.arr).toEqual([
-        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-    ]);
+describe('fors module testing', function () {
+    test('print1To20', () => {
+        const console = {
+            arr: [],
+            log: function (msg) {
+                this.arr.push(msg);
+            },
+        }
+        fors.print1To20(console);
+        expect(console.arr)
+            .toEqual([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+    });
+
+    test('print20To1', () => {
+        const console = {
+            arr: [],
+            log: function (msg) {
+                this.arr.push(msg);
+            },
+        }
+        fors.print20To1(console);
+        expect(console.arr)
+            .toEqual([20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]);
+    });
+
+    test('print1To28Step3', () => {
+        const console = {
+            arr: [],
+            log: function (msg) {
+                this.arr.push(msg);
+            },
+        }
+        fors.print1To28Step3(console);
+        expect(console.arr)
+            .toEqual([1,4,7,10,13,16,19,22,25,28]);
+    });
+
+    test('printFrom30To6Step3Descending', () => {
+        const console = {
+            arr: [],
+            log: function (msg) {
+                this.arr.push(msg);
+            },
+        }
+        fors.printFrom30To6Step3Descending(console);
+        expect(console.arr)
+            .toEqual([30,26,22,18,14,10,6]);
+    });
 });
-
-
